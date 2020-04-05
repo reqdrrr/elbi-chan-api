af2b4cb90fca865713c1e6105b875ba0ec18a918
Requiem Dollars <splint.hub@gmail.com>
2020-04-06 00:47:34 +0800

    Fixed cors header

--- router.js
+++ router.js
@@ -5,7 +5,8 @@ module.exports = (app) => {
     // Allow Cross Origin Resource Sharing
     app.use((req, res, next) => {
       // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-      res.setHeader('Access-Control-Allow-Origin', 'https://elbi-chan-api.herokuapp.com');
+      // res.setHeader('Access-Control-Allow-Origin', 'https://text-ch.web.app');
+      res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Credentials', 'true');
       res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
       res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
